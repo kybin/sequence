9bc52a195b9062b65b280d09957ade58f6a6f849
kim yongbin <kybinz@gmail.com>
2017-08-05 15:25:25 +0900

    add examples.

---
+++ example/basic.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kybin/sequence"
+)
+
+func main() {
+	fi, err := os.Stat("data")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	if !fi.IsDir() {
+		fmt.Fprintln(os.Stderr, "data should be a directory")
+		os.Exit(1)
+	}
+
+	dir, err := os.Open(fi.Name())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer dir.Close()
+
+	filenames, err := dir.Readdirnames(-1)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	man := sequence.NewManager(sequence.NewSplitter(), sequence.FmtSharp)
+	for _, f := range filenames {
+		man.Add(f)
+	}
+
+	fmt.Println(man)
+}
